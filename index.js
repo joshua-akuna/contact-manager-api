8c69ee4d8786681b7f3c5db3e4a806ab0d2328e3
Akuna Joshua <akunajoshua@gmail.com>
2026-01-03 16:31:27 +0100

    postsRoutes file created and initialised

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 // import express
-const express = require('express')
+const express = require('express');
 // create the app server from express
-const app = express()
+const app = express();
 // import and configure dotenv dependency
-require('dotenv').config()
+require('dotenv').config();
+// imports the routes
+const postsRoutes = require('./routes/postsRoutes');
 // declare the PORT variable
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
-// a get api endpoint
-app.get('/api/v1/posts', (req, res)=>{
-    res.status(200).json({"message": "Hello Josh"})
-})
+// app endpoint for all posts
+app.use('/api/v1/posts', postsRoutes);
 
 // app listens on port
-app.listen(PORT, ()=> {
-    console.log(`app listening on port ${PORT}`);
-})
+app.listen(PORT, () => {
+  console.log(`app listening on port ${PORT}`);
+});
